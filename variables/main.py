8b391b5e9f85aca3b6a937b6cd654f40fb199a45
unknown <bekirk0321@gmail.com>
2018-12-09 18:02:38 -0700

    First commit, completed files for homework #1

---
+++ variables/main.py
@@ -0,0 +1,19 @@
+"""
+Homework #1 Variables
+"""
+
+# The song name
+SongName = "Eye of The Tiger"
+# The artist of the song
+Artist 		= "Frankie Sullivan and Jim Peterik"
+# Genre of the song
+Genre  		= "Hard Rock"
+# The name of the label
+Label  		= "Scottie Brothers"
+# The year the song was released
+ReleaseYear = "1982"
+
+print(Artist)
+print(Genre)
+print(Label)
+print(ReleaseYear)
