75ab59e710f3094f3cf02182696703e0864a4bc9
unknown <bekirk0321@gmail.com>
2018-12-09 18:20:36 -0700

    Completed Homework #2, Functions

---
+++ homework_assignments/homework_two_functions/main.py
@@ -0,0 +1,19 @@
+"""
+Homework #2 Functions
+"""
+
+# Returns the Song Name
+def songName():
+	return "Eye of The Tiger"
+
+# Returns the Artist
+def artist():
+	return "Frankie Sullivan and Jim Peterik"
+
+# Returns the Genre
+def genre():
+	return "Hard Rock"
+
+# Extra Credit
+def boolean(): 
+	return True
